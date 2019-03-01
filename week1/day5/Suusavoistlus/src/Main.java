6be7a315187de3c466e81fae2c620afd2da7ed00
Õpilane <opilane@ak.sise>
2019-03-01 16:18:47 +0200

    Suusavoistlus kodutöö jätk

--- week1/day5/Suusavoistlus/src/Main.java
+++ week1/day5/Suusavoistlus/src/Main.java
@@ -2,7 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Võistlus alga!");
+/*
+Kodune ülesanne:
+dopingu kordaja (mõni on lihtsalt parem kui teine)
+Pärsi 0 kiirusega vist ei tohiks keegi sõita
+Keegi sai vigastada ja katkestab (randomlt)
+Joonista võistlus välja.
+*/
 
         new Voistlus();
     }
