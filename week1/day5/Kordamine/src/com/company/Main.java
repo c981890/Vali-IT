fe6268f489323757b0f09474575dd45ee5019b81
Õpilane <opilane@ak.sise>
2019-03-01 13:49:46 +0200

    Kordamine

---
+++ week1/day5/Kordamine/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, World!");
+
+        if (true && false || (1 > 0) && true) {
+            System.out.println("Tõene");
+        } else {
+            System.out.println("Väär");
+        }
+    }
+}
