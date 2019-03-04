24f38fbbc6f2eafa7bcee081f090490e67c7b7be
Õpilane <opilane@ak.sise>
2019-03-04 14:15:16 +0200

    FizzArray

--- week2/day1/Ulesanded1Methods/src/Main.java
+++ week2/day1/Ulesanded1Methods/src/Main.java
@@ -30,6 +30,23 @@ public class Main {
         System.out.println("has22 vastus: " + has22(sums4));
     }
 
+    /*
+    Given a number n, create and return a new int array of length n, containing the numbers 0, 1, 2, ... n-1.
+    The given n may be 0, in which case just return a length 0 array. You do not need a separate if-statement
+    for the length-0 case; the for-loop should naturally execute 0 times in that case, so it just works. The
+    syntax to make a new int array is: new int[desired_length].
+     */
+    public int[] fizzArray(int n) {
+        int[] massiiv = new int[n];
+        for (int i = 0; i < massiiv.length; i++) {
+            massiiv[i] = i;
+
+        }
+        return massiiv;
+
+    }
+
+
     // Given an array of ints, return true if the number of 1's is greater than the number of 4's.
     public boolean more14(int[] nums) {
         int nrOf1 = 0;
