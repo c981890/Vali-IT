68e884032f7242340ff4ceb1fa2cc921afc426cb
Õpilane <opilane@ak.sise>
2019-04-01 16:23:45 +0300

    add

---
+++ week6/stringcalculator/src/main/java/ee/valiit/stringcalculator/StringcalculatorApplication.java
@@ -0,0 +1,16 @@
+package ee.valiit.stringcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringcalculatorApplication.class, args);
+	}
+
+	public static int add(String s) {
+		return 0;
+	}
+}
